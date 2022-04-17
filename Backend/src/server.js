fba6524fa06dec39870c66a5ae5456d67036c349
dustin <nolaxking@gmail.com>
2022-04-16 17:41:49 -0700

    make it better

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,10 +1,10 @@
-const { PORT = 5000 } = process.env;
+///const { PORT = 5000 } = process.env;
 
 const path = require("path");
 const app = require(path.resolve(
   `${process.env.SOLUTION_PATH || ""}`,
   "src/app"
 ));
-
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+const port = process.env.PORT || 5000
+const listener = () => console.log(`Listening on Port ${port}!`);
+app.listen(port, listener);
